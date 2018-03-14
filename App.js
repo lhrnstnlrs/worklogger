8ce291ed8754fffd84580ea4c834b1f269f12603
Lhou <llariosa@teamextension.com>
2018-03-14 13:26:16 +0800

    logs section, list logs, delete log

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, TextInput, Picker, Button } from 'react-native';
-
-let Log = {
-  duration: 0,
-  project: '',
-  remarks: ''
-}
+import { StyleSheet, Text, View, Platform, TextInput, Picker, TouchableHighlight, ScrollView, FlatList, Button } from 'react-native';
 
 class StatusBar extends React.Component{
   render(){
@@ -25,11 +19,60 @@ class Header extends React.Component {
   }
 };
 
-class Body extends React.Component {
+class WorkLogger extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalHours: 0,
+      logs: []
+    };
+
+    this.handleAddLog = this.handleAddLog.bind(this);
+    this.handleDeleteLog = this.handleDeleteLog.bind(this);
+  }
+
+  handleAddLog = (durationValue, projectValue, remarksValue) => {
+    if(isNaN(durationValue)) {
+      alert('Please provide the number of hours spent.')
+    } else if(projectValue == '') {
+      alert('Please select a project.')
+    } else if(remarksValue == '') {
+      alert('Please provide remarks.')
+    } else {
+      let log = {
+        duration: durationValue,
+        project: projectValue,
+        remarks: remarksValue
+      }
+
+      this.setState({
+        totalHours: this.state.totalHours + Number.parseFloat(durationValue),
+        logs: [log, ...this.state.logs]
+      })
+    }
+  }
+
+  handleDeleteLog = (indexValue) => {
+    alert(indexValue)
+    let array = this.state.logs
+    //let splicedArray = array.splice(indexValue, 1)
+    this.setState({
+      totalHours: this.state.totalHours - Number.parseFloat(array[indexValue].duration),
+      logs: array.filter( (item, index) => index !== indexValue)
+    })
+  }
+
   render() {
     return (
       <View style={styles.body}>
-        <NewLogForm />
+        <NewLogForm handleAddLog = {this.handleAddLog} />
+
+        <LogsSection
+          logs = {this.state.logs}
+          totalHours = {this.state.totalHours}
+          handleDeleteLog = {this.handleDeleteLog} />
+
       </View>
     );
   }
@@ -40,7 +83,7 @@ class NewLogForm extends React.Component {
     super(props);
 
     this.state = {
-      durationValue: 0,
+      durationValue: '',
       projectValue: '',
       remarksValue: ''
     };
@@ -58,21 +101,12 @@ class NewLogForm extends React.Component {
     this.setState({ remarksValue: value })
   }
 
-  addLog = () => {
-    if(isNaN(this.state.durationValue)) {
-      alert('Duration must be a number.')
-    } else if(this.state.projectValue == '') {
-      alert('Project must not be blank.')
-    } else {
-      alert('Duration: ' + this.state.durationValue + 'h\nProject: ' + this.state.projectValue + '\nRemarks: ' + this.state.remarksValue)
-    }
-  }
-
   render() {
     return (
-      <View>
+      <ScrollView style={styles.newLogForm} keyboardShouldPersistTaps="handled" scrollEnabled={false}>
         <Text>Duration</Text>
         <TextInput style = {styles.input}
+            keyboardType="numeric"
             underlineColorAndroid = "transparent"
             placeholder = "In Hours"
             placeholderTextColor = "#D6D6D6"
@@ -95,9 +129,45 @@ class NewLogForm extends React.Component {
             placeholderTextColor = "#D6D6D6"
             onChangeText = {this.updateRemarksValue} />
 
-        <Button
-            onPress = {this.addLog}
-            title = "Add Log" />
+        <TouchableHighlight
+            style = {styles.button}
+            onPress = {()=>this.props.handleAddLog(this.state.durationValue, this.state.projectValue, this.state.remarksValue)}
+            underlayColor = '#fff' >
+            <Text style = {styles.buttonText}>Add Log</Text>
+        </TouchableHighlight>
+      </ScrollView>
+      
+    );
+  }
+};
+
+class LogsSection extends React.Component {
+  
+  render() {
+    let logArray = this.props.logs
+    return (
+      <View style = {styles.logSection}>
+        <Text style={{fontSize: 25}}>Logs</Text>
+        <Text>Total log for the day: {this.props.totalHours} hours</Text>
+        <FlatList
+            style = {styles.list}
+            data = {this.props.logs}
+            keyExtractor = {(item, index) => index}
+            renderItem = {({ item, index }) => 
+              <View style={styles.item}>
+                <View style={{flexDirection: 'row', flexWrap:'wrap', marginLeft: 10}}>
+                  <Text style={{fontSize: 18}}>{item.duration}h - {item.project}</Text>
+                  <TouchableHighlight
+                      style = {{marginTop: 3}}
+                      onPress = {()=>this.props.handleDeleteLog(index)}
+                      underlayColor = '#fff' >
+                      <Text style={{color: 'red'}}>  [ Delete ]</Text>
+                  </TouchableHighlight>
+                </View>
+                <Text style={{marginLeft: 10}}>{item.remarks}</Text>
+              </View>
+            }
+        />
       </View>
     );
   }
@@ -109,7 +179,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <StatusBar />
         <Header />
-        <Body />
+        <WorkLogger />
       </View>
     );
   }
@@ -117,7 +187,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   header: {
-    height: 70,
+    height: 60,
     backgroundColor: 'powderblue',
     alignItems: 'center',
     justifyContent: 'center',
@@ -131,7 +201,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   body: {
-    margin: 30,
+    marginTop: 10,
   },
   input: {
     padding: 5,
@@ -146,5 +216,35 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#e2e2e2',
     fontSize: 15,
-  }
+  },
+  button:{
+    marginTop: 10,
+    paddingTop: 6,
+    paddingBottom: 6,
+    backgroundColor: 'powderblue',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  newLogForm: {
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  logSection: {
+    marginTop: 10,
+    borderTopWidth: 2,
+    borderColor: '#e2e2e2',
+  },
+  list: {
+  },
+  item: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    padding: 5
+  },
 });
